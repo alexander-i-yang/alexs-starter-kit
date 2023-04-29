72bc696aaca45d320c7649c9a8248dea1c29b00d
Alex Yang <alex@yang2k.com>
2023-04-29 04:36:11 -0400

    Init commit

---
+++ Alex's Starter Kit/Runtime/UI/OptionsController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ASK.UI
+{
+    public class OptionsController : MonoBehaviour
+    {
+        /*private static Dictionary<Type, ToggleReceiver> _toggleReceivers = new();
+
+        private void OnEnable()
+        {
+            _toggleReceivers.Clear();
+            var receivers = GetComponents<ToggleReceiver>();
+            foreach (var r in receivers)
+            {
+                _toggleReceivers.Add(r.GetType(), r);
+            }
+        }
+        
+        public static ToggleReceiver GetToggleReciever<T>() where T : ToggleReceiver
+        {
+            return _toggleReceivers[typeof(T)];
+        }*/
+    }
+}
