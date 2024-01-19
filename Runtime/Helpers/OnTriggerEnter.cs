16b7f0300e550858f065b0fcc07271fd08cbd898
Alex Yang <alex@yang2k.com>
2024-01-18 20:06:17 -0500

    OnTriggerEnter Update

--- Runtime/Helpers/OnTriggerEnter.cs
+++ Runtime/Helpers/OnTriggerEnter.cs
@@ -9,11 +9,17 @@ public class OnTriggerEnter : MonoBehaviour
     public UnityEvent onEnter;
     public UnityEvent onExit;
 
+    [SerializeField] private bool onlyTriggerOnce;
+    
+    private bool _hasEntered;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
+            if (onlyTriggerOnce && _hasEntered) return;
             onEnter.Invoke();
+            _hasEntered = true;
         }
     }
     private void OnTriggerExit2D(Collider2D other)
